564d88fe7706032611bdc037b0ca28e62c6c6905
Dhrubajyoti <dhrubajyotir.ug25.cs@nitp.ac.in>
2026-04-15 12:22:12 +0530

    comminted

---
+++ calculator.js
@@ -0,0 +1,30 @@
+const display = document.getElementById("display");
+
+function appendValue(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        let expression = display.value;
+
+        expression = expression.replace(
+            /(\d+(\.\d+)?)\s*%\s*(\d+(\.\d+)?)/g,
+            (_, a, __, b) => '(${a}/100)*${b}'
+        );
+
+        expression = expression.replace(/(\d+(\.\d+)?)%/g, "($1/100)");
+
+        display.value = eval(expression);
+    } catch {
+        display.value = "Error";
+    }
+}
